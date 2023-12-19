3092c193f3567ea7c470d16d4c388beb00f17d49
Drew Harris <harrisd@smu.edu>
2023-12-18 20:45:46 -0600

    Fake error on purpose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 export default async function Home() {
   // const hello = await api.post.hello.query({ text: "from tRPC" });
   // const session = await getServerAuthSession();
+  const action = async () => {
+    "use server";
+    throw new Error("Error on purpose");
+  };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div>Test123</div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[#171717] text-white">
+      <div>SMURF RADIO</div>
+      <form action={action}>
+        <button type="submit">Submit Error</button>
+      </form>
     </main>
   );
 }

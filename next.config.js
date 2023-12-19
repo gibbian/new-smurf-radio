ce14385d07165e68fef8b9fb6f672ff6d16f134b
Drew Harris <harrisd@smu.edu>
2023-12-18 20:41:31 -0600

    Add sentry

--- next.config.js
+++ next.config.js
@@ -1,10 +1,38 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
 await import("./src/env.js");
 
-/** @type {import("next").NextConfig} */
-const config = {};
+export default withSentryConfig(
+  {
+    sentry: {
+      widenClientFileUpload: true,
 
-export default config;
+      // Transpiles SDK to be compatible with IE11 (increases bundle size)
+      transpileClientSDK: true,
+
+      // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+      tunnelRoute: "/monitoring",
+
+      // Hides source maps from generated client bundles
+      hideSourceMaps: true,
+
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      disableLogger: true,
+
+      // Enables automatic instrumentation of Vercel Cron Monitors.
+      // See the following for more information:
+      // https://docs.sentry.io/product/crons/
+      // https://vercel.com/docs/cron-jobs
+      automaticVercelMonitors: true,
+    },
+  },
+  {
+    silent: true,
+    org: "drewh-2m",
+    project: "smurf-website",
+  },
+);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,6 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div>Test123</div>
-      <audio
-        src="https://listen.mixlr.com/b56be669394360d179edfe32f99ab02c"
-        controls
-      ></audio>
     </main>
   );
 }
